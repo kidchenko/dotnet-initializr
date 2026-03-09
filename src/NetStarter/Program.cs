b3a00b8304fb3cc0db891ec0ca05c683c5c5977a
kidchenko <kidchenko@gmail.com>
2026-03-09 12:31:46 -0300

    feat(19-01): wire DotNetNewPanel into Home.razor and register service in Program.cs
    - Add <DotNetNewPanel> above <CliCommandPanel> in cli-panel-wrapper div
    - Register DotNetNewCommandService as scoped in Program.cs
    - Existing CliCommandPanel usage unchanged (CLI-02 compliance)

--- src/NetStarter/Program.cs
+++ src/NetStarter/Program.cs
@@ -19,6 +19,7 @@ builder.Services
 builder.Services.AddScoped<FileTreeService>();
 builder.Services.AddScoped<ZipService>();
 builder.Services.AddScoped<CliCommandService>();
+builder.Services.AddScoped<DotNetNewCommandService>();
 builder.Services.AddScoped<ProjectGenerationService>();
 
 builder.Services.AddScoped(sp => new HttpClient
